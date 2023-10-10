f238338449459b9a9e5120ddda795fa69083a98f
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-10 08:13:33 +0000

    string and object and primitive

---
+++ javascript_basic/01_practice.js
@@ -0,0 +1,16 @@
+const number =67
+//console.log(number.toString);
+//console.log(number);
+const number2=new Number(45)
+//console.log(number2.toString);
+console.log(typeof number)
+console.log(typeof number2)
+// const newnumber=number.toString
+// console.log(newnumber)
+const mun=String(number)
+console.log(mun);
+console.log(typeof mun);
+const check=new String("jfij")
+console.log(typeof check);
+const str="jokeo"
+console.log(typeof str);
