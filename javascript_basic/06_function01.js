3608b47d699010e5e8e59e447f552559c70d1447
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-11 11:35:44 +0000

    basic function

---
+++ javascript_basic/06_function01.js
@@ -0,0 +1,18 @@
+// function fun(name)
+// {
+//     return `${name} is logged in`
+// }
+// console.log(fun("apurba"))
+function sum(number1,number2)
+{
+    console.log(number1+number2)
+}
+sum//it is only reference type
+sum(4,5)//it is execution
+sum(2,"1")//21
+sum(null,3)//3
+/*
+in case of java script we do not need to give return type and as well as do not need to mention
+ argument type.
+
+*/
