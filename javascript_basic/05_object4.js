7acbd975966d56b9a1a41ba5f754e1769ddc82ba
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-11 08:03:43 +0000

    destriucture and api talk and json introduction

---
+++ javascript_basic/05_object4.js
@@ -0,0 +1,17 @@
+//destructre of object
+const ob={
+    name:"apurba",
+    age:22,
+    height:"good"
+
+}
+const{name:nam}=ob//destructre of an object it is used to avoid write ob.name every time
+console.log(nam);
+
+//what isd api
+//api is kind of response which is comes as a json file where a object is present without having any name and where a
+//key value is also in a string
+// {
+//     "name":"apurba",
+//     "age":22
+// }
