629ed3232767299d1978b5f23bed04097fb31b60
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-12 07:25:07 +0000

    nco operator

---
+++ javascript_basic/15_ncooperator.js
@@ -0,0 +1,9 @@
+//nullish coalescing operator (??) for null and undefined
+// let val;
+val=12?? 14
+//console.log(val);//12
+val1=null??13
+console.log(val1);//13 because if any number comes from database with null the
+ //second value always assign
+ // second item normaly some function which fetch some value inn case of project
+ // val=  undefined ?? function(){}
