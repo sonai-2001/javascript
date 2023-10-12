7f504b0ff7062eaeef509b1a71f9a2b25256220c
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-12 06:28:31 +0000

    execute

---
+++ javascript_basic/12_javascriptexecutable.js
@@ -0,0 +1,22 @@
+/*
+in case of javascript there are two main executable context
+1. global executable context->>>>  it is under this keyword
+2. function executable context
+** additional  evil executable context which is under global
+
+foe javascript code execution there are twp phrase 
+1. memory phase 
+2. executable phase
+
+memory phase ---> at memory phase all the variable gets its memory and assigned with undefined and 
+the function are assigned with its defination .
+
+executable phase ----> in this phase all the variable gets its value and the for each 
+function execution another memory phase and executable phase gets created  and so on and 
+a return value comes  at the calling position.
+
+
+
+
+
+*/
