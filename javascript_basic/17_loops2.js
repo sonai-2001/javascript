fc4c647643d0b2dd68d2f40eb766fa243b99ee35
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-12 10:53:13 +0000

    loops2

---
+++ javascript_basic/17_loops2.js
@@ -0,0 +1,12 @@
+let arr =["virat", "rohit","rahul","shubman"]
+let i=0
+// while(i<arr.length)
+// {
+//     console.log(`batsman is ${arr[i]} `)
+//     i++
+// }
+do
+{
+    console.log(`batsman is ${arr[i]} `)
+    i++
+}while(i<arr.length);
