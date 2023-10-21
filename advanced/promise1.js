6f29e1194178c73d57fca59a810ff4f5dfcea351
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-10-21 08:15:27 +0000

    promise resolve

---
+++ advanced/promise1.js
@@ -0,0 +1,28 @@
+// setTimeout(function(){
+//     console.log("hi")
+// },2000)
+// console.log("apurba") asynchronus js where some thing is happening after some time but meanwhile 
+//normal code flow is happening.
+
+const promise1=new Promise(function(resolve,reject){
+       setTimeout(function()
+       {
+        console.log("hi")
+        resolve()
+       },1000)
+       
+})
+promise1.then(function(){
+    console.log("done")
+})
+
+
+const promise2=new Promise(function(resolve,reject){
+     setTimeout(function(){
+        console.log("hi")
+        resolve({a:1,b:2})
+     },2000)
+})
+promise2.then(function(obj){
+console.log(obj)
+})
